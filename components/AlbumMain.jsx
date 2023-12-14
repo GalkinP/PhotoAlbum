ba511921cdc734794132fc8363518a9fc626ff32
GalkinP <gpp.spb@mail.ru>
2023-12-14 18:38:55 +0300

    user

--- components/AlbumMain.jsx
+++ components/AlbumMain.jsx
@@ -3,10 +3,10 @@ const Layout = require('./Layout');
 const FormAlbum = require('./FormAlbum');
 const AddNewAlbum = require('./AddNewAlbum');
 
-function AlbumMain({ title, albums, photos }) {
+function AlbumMain({ user, title, albums, photos }) {
   return (
     <Layout title={title}>
-
+<p>{user.name}</p>
       <AddNewAlbum />
       <div className='card_album'>
         {albums && albums.map((album) => <FormAlbum album={album} photos={photos}/>)}
