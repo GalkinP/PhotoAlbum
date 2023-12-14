38ec151edf42a0d03f41bb69b81ddb9977b5be3c
Death960 <moonshine7960@gmail.com.com>
2023-12-14 13:38:21 +0300

    AlbumsMain

---
+++ routes/index.route.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+// view
+const authRouter = require('./views/auth.router');
+const mainPageRouter = require('./views/mainPage.router');
+
+// api
+
+// use
+router.use('/auth', authRouter);
+router.use('/mainPage', mainPageRouter);
+
+module.exports = router;
