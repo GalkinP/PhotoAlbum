e40309204b26c024c06f6ac06c067dbc2164268a
GalkinP <gpp.spb@mail.ru>
2023-12-14 15:50:11 +0300

    count

--- components/AlbumMain.jsx
+++ components/AlbumMain.jsx
@@ -2,11 +2,11 @@ const React = require('react');
 const Layout = require('./Layout');
 const FormAlbum = require('./FormAlbum');
 
-function AlbumMain({ title, albums }) {
+function AlbumMain({ title, albums, photos }) {
   return (
     <Layout title={title}>
       <div className='card_student'>
-        {albums && albums.map((album) => <FormAlbum album={album} />)}
+        {albums && albums.map((album) => <FormAlbum album={album} photos={photos} />)}
       </div>
     </Layout>
   );
--- components/FormAlbum.jsx
+++ components/FormAlbum.jsx
@@ -1,12 +1,14 @@
 const React = require('react');
+const photo = require('../db/models/photo');
 
-function FormAlbum({ album }) {
+function FormAlbum({ album , photos}) {
+ const count = photos.filter(photo => photo.albumID===album.id).length
   return (
     <div className='card' style={{ width: '18rem' }}>
       {/* <img src={student.avatar} className='card-img-top' alt='...' /> */}
       <div className='card-body'>
         <h5 className='card-title'>{album.title}</h5>
-        <p className='card-text'>{album.count}</p>
+        <p className='card-text'>{`В АЛЬБОМЕ: ${count}`}</p>
         <p className='card-text'>{album.isOpen}</p>
         {/* <a href='#' className='btn btn-primary'>
           Go somewhere
