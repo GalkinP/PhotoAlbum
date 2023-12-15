fad26911a0d0f9f94a8e1124aff3f3b4ad927b4b
GalkinP <gpp.spb@mail.ru>
2023-12-15 10:55:28 +0300

    add

--- public/scripts/albumAdd.js
+++ public/scripts/albumAdd.js
@@ -1,16 +1,16 @@
-const formAddAlbum = document.querySelector('.formAddAlbum');
+const formAddAlbum = document.querySelector(".formAddAlbum");
 
 if (formAddAlbum) {
-  formAddAlbum.addEventListener('submit', async (event) => {
+  formAddAlbum.addEventListener("submit", async (event) => {
     event.preventDefault();
-    console.log('script');
+    console.log("script");
     const { title } = event.target;
     try {
-      const res = await fetch('/api/', {
-        method: 'POST',
+      const res = await fetch("/api/", {
+        method: "POST",
         // здесь мы с вами говорим и том, как общаемся, JSON
         headers: {
-          'Content-type': 'application/json',
+          "Content-type": "application/json",
         },
         // положили все в одну коробчку, которая скрыта от глаз посторонних пользователей
         body: JSON.stringify({
@@ -22,14 +22,16 @@ if (formAddAlbum) {
       });
       const data = await res.json();
       console.log(data);
-      if (data.message === 'success') {
+      if (data.message === "success") {
         // очищает форму
         formAddAlbum.reset();
+        const ttt =document.querySelector('.card_album')
+        console.log(ttt, "uhuhuh")
         document
           // находим див, куда хотим добавить карточку
-          .querySelector('.card__album')
+          .querySelector('.card_album')
           // добавляет карточку в конец нашего дива, который мы нашли выше
-          .insertAdjacentHTML('beforeend', data.html);
+          .insertAdjacentHTML("beforeend", data.html);
       }
     } catch ({ message }) {
       console.log(message);
